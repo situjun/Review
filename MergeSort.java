177e4d57634fd377193c17120f22e178f606e0e5
icewating <icewating@gmail.com>
2016-12-07 18:30:10 -0500

    mergesort review

--- MergeSort.java
+++ MergeSort.java
@@ -1,35 +1,39 @@
 class MergeSort{
-	public  static void main(String[] ags){
-		int[] input = {1,2,5,88,4,1,0,2,55,45,21,11,31,11,9,8};
-		int[] tmp = new int[input.length];
-		recursion(input,tmp,0,input.length-1);
-		for(int i:tmp){
+	public static void main(String[] args){
+		int[] arr = {9,8,1,2,2,55,12,87,111,1,0,5,6,7,6,81};
+		int length = arr.length;
+		int[] tmp = new int[length];
+		//Key point:注意整个递归过程可以当成一个整体，所以不用担心深层数组改变却不会改变外层数组的问题
+		Sorting(arr,tmp,0,length-1);
+		System.out.println("Result:");
+		for(int i:arr){
 			System.out.print(i+",");
 		}
 	}
-	public static void recursion(int[] input,int[] tmp,int left,int right){
-		//if(input[left] < input[right]){
-		if(left<right){
-			recursion(input,tmp,left,(left+right)/2);
-			recursion(input,tmp,(left+right)/2+1,right);
-			int index1 = left,index2 = (left+right)/2+1,index3 = left;
-			while(index1<=(left+right)/2 && index2<=right){
-				if(input[index1]<input[index2]){
-					tmp[index3++] = input[index1++];
-				} else {
-					tmp[index3++] = input[index2++];
-				}
-			}
-			while(index1<=(left+right)/2){
-				tmp[index3++] = input[index1++];
-			}
-			while(index2<=right){
-				tmp[index3++] = input[index2++];
-			}
-			//very important!!!!搴旇鍦ㄤ复鏃舵暟缁勫唴鎺掑ソ搴忓悗锛屾妸鏁版嵁鏀惧洖鍘熸暟缁勪腑
-			for(int i=left;i<=right;i++){
-				input[i] = tmp[i];
-			}
+	public static void Sorting(int[] arr,int[] tmp,int low,int high){
+		if(low<high){
+			int mid = (low+high)/2;
+			Sorting(arr,tmp,low,mid);
+			Sorting(arr,tmp,mid+1,high);
+			Merge(arr,tmp,low,mid,high);
 		}
+	}
+	public static void Merge(int[] arr,int[] tmp,int low,int mid,int high){
+		int index1 = low;
+		int index2 = mid+1;
+		int index3 = low;
+		while(index1<=mid && index2<=high){
+			if(arr[index1]<=arr[index2]) tmp[index3++] = arr[index1++];
+			else tmp[index3++] = arr[index2++];
+		}
+		while(index1<=mid){
+			tmp[index3++] = arr[index1++];
+		}
+		while(index2<=high){
+			tmp[index3++] = arr[index2++];
+		}
+		while(low<=high){
+			arr[low] = tmp[low++];
+ 		}
 	}
 }
