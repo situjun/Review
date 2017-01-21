089dc1c2625dfb9010b5ba0a07e5ebebce769e69
icewating <icewating@gmail.com>
2017-01-21 17:36:34 +0800

    Permutations

---
+++ 0120/Permutations.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Permutations{
+	public static void main(String[] args){
+		int[] arr = {1,2,3};
+		List<List<Integer>> list = permute(arr);
+		for(List<Integer> i:list){
+			for(Integer integer:i){
+				System.out.print(integer+"\t");
+			}
+			System.out.print("\n");
+		}
+	}
+	public static List<List<Integer>> permute(int[] nums) {
+	   List<List<Integer>> list = new ArrayList<>();
+	   // Arrays.sort(nums); // not necessary
+	   backtrack(list, new ArrayList<>(), nums);
+	   return list;
+	}
+
+	private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+	   if(tempList.size() == nums.length){
+		  list.add(new ArrayList<>(tempList));
+	   } else{
+		  for(int i = 0; i < nums.length; i++){ 
+			 if(tempList.contains(nums[i])) continue; // element already exists, skip
+			 tempList.add(nums[i]);
+			 backtrack(list, tempList, nums);
+			 tempList.remove(tempList.size() - 1);
+		  }
+	   }
+	}
+}
