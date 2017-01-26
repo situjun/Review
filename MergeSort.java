a81ae63d0757579cd3194cc01ec8d9ffe7a3f439
icewating <icewating@gmail.com>
2017-01-26 22:33:14 +0800

    merge sort review

--- MergeSort.java
+++ MergeSort.java
@@ -1,39 +1,30 @@
 class MergeSort{
 	public static void main(String[] args){
-		int[] arr = {9,8,1,2,2,55,12,87,111,1,0,5,6,7,6,81};
-		int length = arr.length;
-		int[] tmp = new int[length];
-		//Key point:注意整个递归过程可以当成一个整体，所以不用担心深层数组改变却不会改变外层数组的问题
-		Sorting(arr,tmp,0,length-1);
-		System.out.println("Result:");
-		for(int i:arr){
-			System.out.print(i+",");
-		}
+		int[] arr = {5,8,2,3,8,2,8,3,4,7,0,5,3,1,7,2,0,52,4};
+		new MergeSort().sort(arr);
+		for(int i:arr) System.out.print(i+",");
 	}
-	public static void Sorting(int[] arr,int[] tmp,int low,int high){
+	public void sort(int[] arr){
+		int low = 0,high = arr.length-1;
+		int[] tmp = new int[arr.length];
+		sort(arr,tmp,low,high);
+	}
+	public void sort(int[] arr,int[] tmp,int low,int high){
+		int mid = (low+high)/2;
 		if(low<high){
-			int mid = (low+high)/2;
-			Sorting(arr,tmp,low,mid);
-			Sorting(arr,tmp,mid+1,high);
-			Merge(arr,tmp,low,mid,high);
+			sort(arr,tmp,low,mid);
+			sort(arr,tmp,mid+1,high);
+			merge(arr,tmp,low,mid,high);
 		}
 	}
-	public static void Merge(int[] arr,int[] tmp,int low,int mid,int high){
-		int index1 = low;
-		int index2 = mid+1;
-		int index3 = low;
+	public void merge(int[] arr,int[] tmp,int low,int mid,int high){
+		int index1 = low,index3 = low,index2 = mid+1;
 		while(index1<=mid && index2<=high){
-			if(arr[index1]<=arr[index2]) tmp[index3++] = arr[index1++];
+			if(arr[index1] <= arr[index2]) tmp[index3++] = arr[index1++];
 			else tmp[index3++] = arr[index2++];
 		}
-		while(index1<=mid){
-			tmp[index3++] = arr[index1++];
-		}
-		while(index2<=high){
-			tmp[index3++] = arr[index2++];
-		}
-		while(low<=high){
-			arr[low] = tmp[low++];
- 		}
+		while(index1<=mid) tmp[index3++] = arr[index1++];
+		while(index2<=high) tmp[index3++] = arr[index2++];
+		while(low<=high) arr[low] = tmp[low++];
 	}
 }
