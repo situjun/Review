c10fbce5ce1535d30cdc798fbe741a68cfee9869
icewating <icewating@gmail.com>
2017-02-07 00:04:33 +0800

    StringTest

---
+++ 0206/StringTest.java
@@ -0,0 +1,11 @@
+class StringTest{
+	public static void main(String[] s){
+		int[] nums = {1,1};
+		String result = "";
+		for(int i:nums) System.out.println(i+"qq");
+		for(int i:nums) result = i+"";
+        System.out.println(result+"dd");
+		
+		
+	}
+}
